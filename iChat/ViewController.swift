02d88b31073d7eed3d48c0307ed80c102365283c
Jinisha Savani <jinisha@jinishas-macbook-pro.local>
2018-11-01 11:50:02 -0400

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Jinisha Savani on 11/1/18.
+//  Copyright © 2018 Jinisha Savani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
